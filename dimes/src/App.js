c250dc8d9547e6640a0f9b311d9fae79a0d034a4
Hannah <hsistrunk@gmail.com>
2020-06-08 15:55:52 -0400

    create-react-app setup

---
+++ dimes/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Hello world
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
