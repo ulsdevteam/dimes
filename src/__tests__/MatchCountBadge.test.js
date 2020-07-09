86b38ea89d0415f074262e9184f8f6d7a4fc830e
Hillel Arnold <helrond@hotmail.com>
2020-07-09 16:35:25 -0400

    remove MatchCountBadge

--- src/__tests__/MatchCountBadge.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MatchCountBadge from '../components/MatchCountBadge';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MatchCountBadge onClick={function(){}} label="This is a badge" />, div);
-});
