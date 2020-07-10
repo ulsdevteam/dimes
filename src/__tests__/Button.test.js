80e88d9e7a93dc0bd40ee8857a9696a9838264bc
Hillel Arnold <helrond@hotmail.com>
2020-07-10 11:34:43 -0400

    simplify test

--- src/__tests__/Button.test.js
+++ src/__tests__/Button.test.js
@@ -1,60 +1,8 @@
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
-
+import { render } from "react-dom";
 import Button from "../components/Button";
 
-let container = null;
-const originalConsoleError = global.console.error
-
-class PropTypesError extends Error {
-  constructor(message) {
-    super(message); // (1)
-    this.name = "PropTypesError"; // (2)
-  }
-}
-
-beforeEach(() => {
-  container = document.createElement("div");
-  document.body.appendChild(container);
-  global.console.error = (...args) => {
-    const propTypeFailures = [/Failed prop type/, /Warning: Received/]
-
-    if (propTypeFailures.some(p => p.test(args[0]))) {
-      throw new PropTypesError(args[0])
-    }
-
-    originalConsoleError(...args)
-  }
-});
-
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it("throws propType errors", () => {
-  const a = () => {
-    render(<Button label="foo" className="btn" />, container)
-  };
-  expect(a).toThrow(PropTypesError)
-
-  const b = () => {
-    render(<Button buttonType="foo" handleClick={function(){}} />)
-  }
-  expect(b).toThrow(PropTypesError)
-});
-
-
-it("renders with or without a label", () => {
-  act(() => {
-    render(<Button label="foo" buttonType="submit" handleClick={function(){}} />, container);
-  });
-  expect(container.textContent).toBe(" foo");
-
-  act(() => {
-    render(<Button buttonType="reset" handleClick={function(){}} />, container);
-  });
-  expect(container.textContent).toBe(" ");
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  render(<Button type="submit" handleClick={function(){}} />, div);
 });
