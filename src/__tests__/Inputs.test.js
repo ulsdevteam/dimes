ec2e99c554a54b452672745b48b042df2384b34a
Hillel Arnold <helrond@hotmail.com>
2020-07-10 13:14:45 -0400

    removes subclassing, adds YearInput

--- src/__tests__/Inputs.test.js
+++ src/__tests__/Inputs.test.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import { CheckBox } from "../components/Inputs";
+import { CheckBox, YearInput } from "../components/Inputs";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   render(<CheckBox label="Do you want to proceed?" id="1" />, div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  render(<YearInput label="Do you want to proceed?" id="1" />, div);
 });
--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -2,40 +2,20 @@ import React, { Component } from "react";
 import PropTypes from 'prop-types';
 
 
-class Input extends Component {
-  render() {
-    return (
-      <input className={this.props.className} type={this.props.type} id={this.props.id} onClick={this.props.handleClick} />
-    )
-  }
-}
-
-Input.propTypes = {
-  className: PropTypes.string,
-  handleClick: PropTypes.func,
-  id: PropTypes.string.isRequired
-};
-
-class InputLabel extends Component {
-  render() {
-    return (
-      <label className={this.props.className} htmlFor={this.props.id} >{this.props.label}</label>
-    )
-  }
-}
-
-InputLabel.propTypes = {
-  className: PropTypes.string,
-  id: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired
-};
-
 export class CheckBox extends Component {
   render() {
     return (
       <div>
-        <Input className={this.props.className} type="checkbox" id={this.props.id} onClick={this.props.handleClick} />
-        <InputLabel label={this.props.label} className={this.props.className} id={this.props.id} />
+        <input
+          type="checkbox"
+          className={this.props.className}
+          id={this.props.id}
+          onClick={this.props.handleClick} />
+        <label
+          className={this.props.className}
+          id={this.props.id}>
+            {this.props.label}
+        </label>
       </div>
     )
   }
@@ -44,6 +24,30 @@ export class CheckBox extends Component {
 CheckBox.propTypes = {
   className: PropTypes.string,
   handleClick: PropTypes.func,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired
+};
+
+export class YearInput extends Component {
+  constructor(props) {
+    super(props);
+    this.currentYear = new Date().getFullYear();
+  }
+  render() {
+    return (
+      <div>
+        <label className={this.props.className} id={this.props.id} >
+          {this.props.label}
+        </label>
+        <input type="number" className={this.props.className} id={this.props.id} name={this.props.id} onClick={this.props.handleClick} min="0" max={this.currentYear} />
+      </div>
+    )
+  }
+}
+
+YearInput.propTypes = {
+  className: PropTypes.string,
+  handleClick: PropTypes.func,
+  id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired
 };
