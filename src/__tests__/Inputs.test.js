0aee61ccb1c8d181fc2768c6a6491f4ff2ae02de
Hillel Arnold <helrond@hotmail.com>
2020-07-10 14:36:31 -0400

    add Select, TextArea and Text inputs

--- src/__tests__/Inputs.test.js
+++ src/__tests__/Inputs.test.js
@@ -1,25 +1,62 @@
 import React from "react";
 import { render } from "react-dom";
-import { CheckBox, DatePicker, RadioGroup, RadioInput, YearInput } from "../components/Inputs";
+import {
+  CheckBoxInput,
+  DatePickerInput,
+  RadioGroup,
+  RadioInput,
+  SelectInput,
+  SelectOption,
+  TextAreaInput,
+  TextInput,
+  YearInput } from "../components/Inputs";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  render(<CheckBox label="Do you want to proceed?" id="1" />, div);
+  render(<CheckBoxInput label="Do you want to proceed?" id="1" />, div);
 });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  render(<DatePicker label="Select a date" id="1" />, div);
+  render(<DatePickerInput label="Select a date" id="1" />, div);
 });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  render(<RadioGroup label="Select an option" />, div);
+  render(
+    <RadioGroup label="Select an option">
+      <RadioInput label="Foo" id="1" />
+      <RadioInput label="Bar" id="2" />
+    </RadioGroup>, div);
 });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   render(<RadioInput label="Foo" id="1" />, div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  render(
+    <SelectInput label="Select an option" id="1">
+      <SelectOption label="Foo" />
+      <SelectOption label="Bar" />
+    </SelectInput>, div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  render(<SelectOption label="Foo" id="1" />, div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  render(<TextAreaInput label="Foo" id="1" />, div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  render(<TextInput label="Foo" id="1" />, div);
 });
 
 it('renders without crashing', () => {
--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -2,18 +2,16 @@ import React, { Component } from "react";
 import PropTypes from 'prop-types';
 
 
-export class CheckBox extends Component {
+export class CheckBoxInput extends Component {
   render() {
     return (
-      <div>
+      <div className={this.props.className}>
         <input
           type="checkbox"
-          className={this.props.className}
           id={this.props.id}
           name={this.props.id}
           onChange={this.props.handleChange} />
         <label
-          className={this.props.className}
           htmlFor={this.props.id}>
             {this.props.label}
         </label>
@@ -22,30 +20,26 @@ export class CheckBox extends Component {
   }
 }
 
-CheckBox.propTypes = {
+CheckBoxInput.propTypes = {
   className: PropTypes.string,
   handleChange: PropTypes.func,
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired
 };
 
-export class DatePicker extends Component {
-  constructor(props) {
-    super(props);
-    this.currentDay = new Date();
-  }
+export class DatePickerInput extends Component {
   render() {
     return (
-      <div>
-        <label className={this.props.className} htmlFor={this.props.id} >
-          {this.props.label}
+      <div className={this.props.className}>
+        <label
+          htmlFor={this.props.id} >
+            {this.props.label}
         </label>
         <input
           type="date"
-          className={this.props.className}
           id={this.props.id}
           max={this.props.max}
-          min={this.currentDay}
+          min={this.props.min}
           name={this.props.id}
           onChange={this.props.handleChange}
           defaultValue={this.currentDay} />
@@ -54,19 +48,24 @@ export class DatePicker extends Component {
   }
 }
 
-DatePicker.propTypes = {
+DatePickerInput.propTypes = {
   className: PropTypes.string,
+  defaultValue: PropTypes.instanceOf(Date),
   handleChange: PropTypes.func,
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  max: PropTypes.string,
-  defaultValue: PropTypes.string
+  max: PropTypes.instanceOf(Date),
+  min: PropTypes.instanceOf(Date)
 };
+
+DatePickerInput.defaultProps = {
+  min: new Date()
+}
 
 export class RadioInput extends Component {
   render() {
     return (
-      <div>
+      <div className={this.props.className}>
         <input
           type="radio"
           id={this.props.id}
@@ -84,9 +83,10 @@ export class RadioInput extends Component {
 
 RadioInput.propTypes = {
   className: PropTypes.string,
+  groupName: PropTypes.string,
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  onChange: PropTypes.func
+  handleChange: PropTypes.func
 };
 
 export class RadioGroup extends Component {
@@ -105,23 +105,123 @@ RadioGroup.propTypes = {
   label: PropTypes.string.isRequired
 };
 
-export class YearInput extends Component {
-  constructor(props) {
-    super(props);
-    this.currentYear = new Date().getFullYear();
+export class SelectInput extends Component {
+  render()  {
+    return (
+      <div className={this.props.className}>
+        <label
+          htmlFor={this.props.id}>
+            {this.props.label}
+        </label>
+        <select
+          name={this.props.id}
+          id={this.props.id}>
+            {this.props.children}
+        </select>
+      </div>
+    )
   }
+}
+
+SelectInput.propTypes = {
+  className: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired
+}
+
+export class SelectOption extends Component {
   render() {
     return (
-      <div>
-        <label className={this.props.className} htmlFor={this.props.id} >
+      <option
+        value={this.props.value ? this.props.value : this.props.label.toLowerCase()}>
           {this.props.label}
+      </option>
+    )
+  }
+}
+
+SelectOption.propTypes = {
+  label: PropTypes.string.isRequired
+}
+
+export class TextAreaInput extends Component {
+  render()  {
+    return (
+      <div className={this.props.className}>
+        <label
+          htmlFor={this.props.id}>
+            {this.props.label}
+        </label>
+        <textarea
+          id={this.props.id}
+          name={this.props.id}
+          rows={this.props.rows}
+          cols={this.props.cols}>
+        </textarea>
+      </div>
+    )
+  }
+}
+
+TextAreaInput.propTypes = {
+  className: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  rows: PropTypes.number,
+  cols: PropTypes.number
+}
+
+TextAreaInput.defaultProps = {
+  rows: 5,
+  cols: 33
+}
+
+export class TextInput extends Component {
+  render()  {
+    return (
+      <div className={this.props.className}>
+        <label
+          htmlFor={this.props.id}>
+            {this.props.label}
+        </label>
+        <input
+          type="text"
+          id={this.props.id}
+          name={this.props.id}
+          minLength={this.props.minLength}
+          maxLength={this.props.maxLength}
+          size={this.props.size} />
+       </div>
+    )
+  }
+}
+
+TextInput.propTypes = {
+  className: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  minLength: PropTypes.number,
+  maxLength: PropTypes.number,
+  size: PropTypes.number
+};
+
+TextInput.defaultProps = {
+  maxLength: 255,
+  size: 10
+}
+
+export class YearInput extends Component {
+  render() {
+    return (
+      <div className={this.props.className} >
+        <label
+          htmlFor={this.props.id}>
+            {this.props.label}
         </label>
         <input
           type="number"
-          className={this.props.className}
           id={this.props.id}
           max={this.currentYear}
-          min="0"
+          min={this.props.min}
           name={this.props.id}
           onChange={this.props.handleChange} />
       </div>
@@ -134,4 +234,11 @@ YearInput.propTypes = {
   handleChange: PropTypes.func,
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  max: PropTypes.number,
+  min: PropTypes.number
 };
+
+YearInput.defaultProps = {
+  max: new Date().getFullYear(),
+  min: 0
+}
