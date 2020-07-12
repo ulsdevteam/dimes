9792a29b7aad206c99feee93b297cd1227b53b70
Hillel Arnold <helrond@hotmail.com>
2020-07-12 18:30:57 -0400

    adds tile component and tests

---
+++ src/components/Tile.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+
+class HitCount extends Component {
+  render() {
+    return (
+      <span className="hitcount">{this.props.hits} matches</span>
+    )
+  }
+}
+
+class Tile extends Component {
+  render() {
+    return (
+      <li className="tile">
+        {this.props.hits ? (<HitCount hits={this.props.hits} />) : null}
+        {this.props.typeLabel}
+        {this.props.title}
+      </li>
+    )
+  }
+}
+
+class TileList extends Component {
+  constructor(props) {
+    super(props);
+    const items = this.props.items
+    const typeClass = "`{this.itemType}__label`"
+    const listItems = items.map((item) =>
+      <Tile
+        key={item.id}
+        hits={item.hits}
+        title={item.title}
+        typeLabel="`<span className={this.typeClass}>{item.type}<span>`" />
+    );
+  }
+  render() {
+    return (
+      <ul>
+        {this.listItems}
+      </ul>
+    )
+  }
+}
+
+export default TileList;
