f704513726dabc921eca7d84319913fa213460b0
Hillel Arnold <helrond@hotmail.com>
2020-07-20 12:32:38 -0400

    initial styling of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,17 @@ import "./Header.scss";
 class Header extends Component {
   render() {
     return (
-      <header>
-        <a href="/" className="header__brand">dimes.rockarch.org</a>
-        <p className="header__subtitle">The Online Collection and Catalog of Rockefeller Archive Center</p>
-        <Nav className="header__nav" ariaLabel="Main">
-          <NavItem href="#" label="Sign in to RACcess" icon="&rarr;" />
-          <NavItem href="#" label="My List" icon="&rarr;" />
-        </Nav>
+      <header className="rac-header-secondary">
+        <div className="container">
+          <div className="header__brand">
+            <a href="/" class="title">dimes.rockarch.org</a>
+            <p className="subtitle">The Online Collection and Catalog of Rockefeller Archive Center</p>
+          </div>
+          <Nav className="header__nav" ariaLabel="Main">
+            <NavItem href="#" label="Sign in to RACcess" icon="&rarr;" />
+            <NavItem href="#" label="My List" icon="&rarr;" />
+          </Nav>
+        </div>
       </header>
     )
   }
