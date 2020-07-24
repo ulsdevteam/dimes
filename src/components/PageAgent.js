beae5b2db1cf8128ccc345a42a25ce5c6c2c1467
Hillel Arnold <helrond@hotmail.com>
2020-07-24 18:14:34 -0400

    set up basic fetch and redirection

--- src/components/PageAgent.js
+++ src/components/PageAgent.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import PageNotFound from "./PageNotFound"
 
-const PageAgent = () => (
- <div>
-    <h1>Person, Organization, or Family</h1>
-</div>
-)
+class PageAgent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      found: true,
+      title: ""
+    };
+  };
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3000/foo/${this.props.match.params.id}`)
+      .then(res => this.setState({ activeMap: res.data }))
+      .catch(err => this.setState({ found: false }));
+  };
+  render() {
+    return (
+      this.state.found ?
+        (<div>
+            <h1>Person, Organization, or Family</h1>
+          </div>) :
+        (<PageNotFound />)
+    )
+  }
+}
 
 export default PageAgent;
