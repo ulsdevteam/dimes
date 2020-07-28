df8050c3504fda4c605435024beb900f5981eb97
Hillel Arnold <helrond@hotmail.com>
2020-07-28 09:37:20 -0400

    create overall page structure and components

--- src/components/PageAgent.js
+++ src/components/PageAgent.js
@@ -1,28 +1,80 @@
 import React, { Component } from "react";
 import axios from "axios";
-import PageNotFound from "./PageNotFound"
+import PageNotFound from "./PageNotFound";
+import TileList from "./Tile";
+import Button from "./Button";
+
+class AgentDescription extends Component {
+  render() {
+    // TODO: some conditional rendering here
+    return (
+      <div className="agent__description">
+        <h2 className="agent__section-title">Summary</h2>
+        <div className="agent__attributes"></div>
+        <div className="agent__notes"></div>
+      </div>
+    )
+  }
+}
+
+class AgentRelatedCollections extends Component {
+  render() {
+    if (!this.props.collections) {
+      return null;
+    }
+    return (
+      <div className="agent__related">
+        <h2 className="agent__section-title">Related Collections</h2>
+        <TileList items={this.props.collections} />
+        <Button label="Search More Related Collections" />
+      </div>
+    )
+  }
+}
+
+class AgentSidebar extends Component {
+  render() {
+    if (!this.props.agents) {
+      return null;
+    }
+    return (
+      <div className="agent__sidebar">
+        <h2 className="agent__section-title">Related People and Organizations</h2>
+        <TileList items={this.props.agents} />
+      </div>
+    )
+  }
+}
 
 class PageAgent extends Component {
   constructor(props) {
     super(props);
     this.state = {
       found: true,
-      title: ""
+      data: {}
     };
   };
   componentDidMount() {
     axios
-      .get(`http://localhost:3000/foo/${this.props.match.params.id}`)
-      .then(res => this.setState({ activeMap: res.data }))
+      .get(`http://localhost:8000/agents/${this.props.match.params.id}`)
+      .then(res => this.setState({ data: res.data }))
       .catch(err => this.setState({ found: false }));
   };
   render() {
+    if (!this.state.found) {
+      return (<PageNotFound />)
+    }
     return (
-      this.state.found ?
-        (<div>
-            <h1>Person, Organization, or Family</h1>
-          </div>) :
-        (<PageNotFound />)
+      <div className="container agent">
+        <Button label="Back to Search" />
+        <div className="main" role="main">
+          <h1>{ this.state.data.title }</h1>
+          <p className="agent__subtitle"></p>
+          <AgentDescription agent={this.state.data} />
+          <AgentRelatedCollections collections={this.state.data.collections} />
+        </div>
+        <AgentSidebar related={this.state.data.agents} />
+      </div>
     )
   }
 }
