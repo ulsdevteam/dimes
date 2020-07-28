72b7ac1bfcecb5d932815a55cd893abeca3f1e7d
Hannah <hsistrunk@gmail.com>
2020-07-28 16:32:50 -0400

    add 404 page content and styles

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 const PageNotFound = () => (
- <div>
-    <h1>Sorry, the requested page was not found!</h1>
-    <p>Try using the navigation at the top of this page to find what you're looking for.</p>
-</div>
+
+<main id="main" role="main">
+    <div className="container">
+        <div className="wrapper">
+            <h1>404: page not found</h1>
+        </div>
+    </div>
+    <div className="container">
+        <div ClassName="wrapper">
+            <div className="page-content one-column-content">
+                <h2>Sorry, the requested page was not found!</h2>
+                <p>Try using the navigation at the top of this page to find what you're looking for.</p>
+            </div>
+        </div>
+    </div>
+</main>
+
 )
 
 export default PageNotFound;
