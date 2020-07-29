afd77c35999d0f6b86cddb2be3566985b4602027
Hillel Arnold <helrond@hotmail.com>
2020-07-29 15:59:05 -0400

    styling tweaks

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -3,19 +3,15 @@ import React from 'react';
 const PageNotFound = () => (
 
 <main id="main" role="main">
-    <div className="container">
-        <div className="wrapper">
-            <h1>404: page not found</h1>
-        </div>
+  <div className="container">
+    <div className="wrapper">
+      <div className="not-found">
+        <h1>404: page not found</h1>
+        <h2>Sorry, the requested page was not found!</h2>
+        <p>Use the back button on your browser or the navigation at the top of this page to find what you're looking for.</p>
+      </div>
     </div>
-    <div className="container">
-        <div ClassName="wrapper">
-            <div className="one-column-content">
-                <h2>Sorry, the requested page was not found!</h2>
-                <p>Use the back button on your browser or the navigation at the top of this page to find what you're looking for.</p>
-            </div>
-        </div>
-    </div>
+  </div>
 </main>
 
 )
