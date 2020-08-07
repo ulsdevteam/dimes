c4efbb72f0bbbc0452e0cccebcc7d4d793682bd7
Hillel Arnold <helrond@hotmail.com>
2020-08-07 15:47:47 -0400

    improving Button component and styles

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -6,8 +6,8 @@ import "./styles.scss"
 class Button extends Component {
   render() {
     return (
-      <button type={this.props.type} className={this.props.className} onClick={this.props.handleClick} aria-label={this.props.ariaLabel}>
-        {this.props.iconBefore && this.props.iconBefore} {this.props.label} {this.props.iconAfter && this.props.iconAfter}
+      <button type={this.props.type} className={`btn ${this.props.className}`} onClick={this.props.onClick} aria-label={this.props.ariaLabel}>
+        {this.props.iconBefore && <i className="material-icons">{this.props.iconBefore}</i>} {this.props.label} {this.props.iconAfter && <i className="material-icons">{this.props.iconAfter}</i>}
       </button>
     )
   }
@@ -16,7 +16,7 @@ class Button extends Component {
 Button.propTypes = {
   type: PropTypes.oneOf(['button', 'submit', 'reset']),
   className: PropTypes.string,
-  handleClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
   ariaLabel: PropTypes.string,
   iconAfter: PropTypes.string,
   iconBefore: PropTypes.string,
