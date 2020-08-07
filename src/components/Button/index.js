99e78f2553c1363749370b152d2a04a5f2bfe8fa
Hillel Arnold <helrond@hotmail.com>
2020-08-07 16:42:58 -0400

    styling tweaks

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -6,8 +6,8 @@ import "./styles.scss"
 class Button extends Component {
   render() {
     return (
-      <button type={this.props.type} className={`btn #{this.props.className}`} onClick={this.props.handleClick} aria-label={this.props.ariaLabel}>
-        {this.props.iconBefore && this.props.iconBefore} {this.props.label} {this.props.iconAfter && this.props.iconAfter}
+      <button type={this.props.type} className={`btn ${this.props.className}`} onClick={this.props.handleClick} aria-label={this.props.ariaLabel}>
+        {this.props.iconBefore && <i className="material-icons">{this.props.iconBefore}</i>} {this.props.label} {this.props.iconAfter && this.props.iconAfter}
       </button>
     )
   }
