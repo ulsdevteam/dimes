ac4b5407a81ddf0e3f108cf642dfd685d32cf66b
Hillel Arnold <helrond@hotmail.com>
2020-08-07 16:51:10 -0400

    cleaning  up formatting

--- src/components/PageHome/index.js
+++ src/components/PageHome/index.js
@@ -11,36 +11,36 @@ class PageHome extends Component {
         // TODO: implement page background-color $light-grayish-yellow
         return (
             <div className="container">
-                <div className="hero">
-                    <div className="hero__logo">
-                        <img alt={" "} src={logo} />
-                    </div>
-                    <h1 className="hero__text">
-                        Search Our Collections.<br />Discover People and Organizations.<br />Access Digital Content.
-                    </h1>
+              <div className="hero">
+                  <div className="hero__logo">
+                      <img alt={" "} src={logo} />
+                  </div>
+                  <h1 className="hero__text">
+                      Search Our Collections.<br />Discover People and Organizations.<br />Access Digital Content.
+                  </h1>
+              </div>
+              <form role="search" action="/search" method="get">
+                <div className="search search--home">
+                  <div className="input-group__search">
+                    <TextInput
+                        className="label-hidden input__search" //we can also use aria-label with no label element instead of a hidden label, but would need to alter the component
+                        label="Enter a search term"
+                        id="query"
+                        placeholder="Search..."
+                        size="60"
+                        type="search"
+                    />
+                    {/* Add Menu component */}
+                    {/* I think this is just a select that has an ID of `type`? */}
+                    <Button
+                        className="btn--block btn--search"
+                        type="submit"
+                        aria-label="Submit search"
+                        iconBefore="search"
+                    />
                 </div>
-                  <form role="search" action="/search" method="get">
-                  <div className="search search--home">
-                    <div className="input-group__search">
-                      <TextInput
-                          className="label-hidden input__search" //we can also use aria-label with no label element instead of a hidden label, but would need to alter the component
-                          label="Enter a search term"
-                          id="query"
-                          placeholder="Search..."
-                          size="60"
-                          type="search"
-                      />
-                      {/* Add Menu component */}
-                      {/* I think this is just a select that has an ID of `type`? */}
-                      <Button
-                          className="btn--block btn--search"
-                          type="submit"
-                          aria-label="Submit search"
-                          iconBefore="search"
-                      />
-                    </div>
-                    </div>
-                </form>
+              </div>
+            </form>
           </div>
         )
     }
