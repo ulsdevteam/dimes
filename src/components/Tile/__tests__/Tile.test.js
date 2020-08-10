a9cef1e0c9f6178c394d5aa5a70c79b840de5639
Hillel Arnold <helrond@hotmail.com>
2020-08-10 09:47:40 -0400

    additional tile list debugging

--- src/components/Tile/__tests__/Tile.test.js
+++ src/components/Tile/__tests__/Tile.test.js
@@ -5,7 +5,7 @@ import TileList from "../../Tile"
 
 const items = [
   {"id": 1, "title": "item 1", "type": "collection", "dates": {"expression": "1990-1991", "start": "1990-01-01", "begin": "1991-12-31"}},
-  {"id": 2, "title": "item 2", "hits": 4},
+  {"id": 2, "title": "item 2", "hits": 4, "dates": []},
   {"id": 3, "title": "item 3", "type": "person"}
 ];
 
