3998261eda8bd0458a1b93180cdc95ae0ef31849
Hannah <hsistrunk@gmail.com>
2020-08-17 19:04:59 -0400

    move hero styles to component

--- src/components/PageHome/index.js
+++ src/components/PageHome/index.js
@@ -4,15 +4,15 @@ import Search from "../Search"
 import "./styles.scss";
 
 class PageHome extends Component {
-    render() {
-        // TODO: implement page background-color $light-grayish-yellow
-        return (
-            <div className="container">
-              <Hero />
-              <Search className="search--home"/>
-          </div>
-        )
-    }
+  render() {
+    // TODO: implement page background-color $light-grayish-yellow
+    return (
+      <div className="container">
+        <Hero />
+        <Search className="search--home"/>
+      </div>
+    )
+  }
 }
 
 export default PageHome;
