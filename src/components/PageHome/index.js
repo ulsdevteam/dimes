a4ed28cbba21d905d7667708fcb51d1a57188110
Hannah <hsistrunk@gmail.com>
2020-08-17 18:38:08 -0400

    create search component

--- src/components/PageHome/index.js
+++ src/components/PageHome/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import Button from "../Button"
+import Search from "../Search"
 import logo from "../../assets/homepage_logo.png"
-import { SelectInput, SelectOption, TextInput } from "../Inputs";
 import "./styles.scss";
 
 class PageHome extends Component {
@@ -18,34 +17,7 @@ class PageHome extends Component {
                       Search Our Collections.<br />Discover People and Organizations.<br />Access Digital Content.
                   </h1>
               </div>
-              <form role="search" action="/search" method="get">
-                <div className="search search--home">
-                  <div className="input-group__search">
-                    <TextInput
-                        className="hide-label input__search" //we can also use aria-label with no label element instead of a hidden label, but would need to alter the component
-                        label="Enter a search term"
-                        id="query"
-                        placeholder="Search..."
-                        size="60"
-                        type="search"
-                    />
-                    <div class="select__search--wrapper">
-                      <SelectInput className="hide-label select__search" id="type" label="Choose a search category">
-                        <SelectOption label="Everything" />
-                        <SelectOption label="Collections" />
-                        <SelectOption label="People" />
-                        <SelectOption label="Organizations" />
-                      </SelectInput>
-                    </div>
-                    <Button
-                        className="btn--search"
-                        type="submit"
-                        aria-label="Submit search"
-                        iconBefore="search"
-                    />
-                </div>
-              </div>
-            </form>
+              <Search className="search--home"/>
           </div>
         )
     }
---
+++ src/components/Search/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Button from "../Button"
+import PropTypes from "prop-types";
+import { SelectInput, SelectOption, TextInput } from "../Inputs";
+import "./styles.scss";
+
+
+class Search extends Component {
+  render () {
+    return (
+      <form role="search" action="/search" method="get">
+        <div className={`search ${this.props.className}`}>
+          <div className="input-group__search">
+            <TextInput
+              className="hide-label input__search"
+              label="Enter a search term"
+              id="query"
+              placeholder="Search..."
+              size="60"
+              type="search"
+            />
+            <div class="select__search--wrapper">
+              <SelectInput className="hide-label select__search" id="type" label="Choose a search category">
+              <SelectOption label="Everything" />
+              <SelectOption label="Collections" />
+              <SelectOption label="People" />
+              <SelectOption label="Organizations" />
+              </SelectInput>
+              </div>
+            <Button
+              className="btn--search"
+              type="submit"
+              aria-label="Submit search"
+              iconBefore="search"
+            />
+          </div>
+        </div>
+      </form>
+    )
+  }
+}
+
+Search.propTypes = {
+  className: PropTypes.string,
+}
+
+export default Search;
