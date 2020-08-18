90353744f9092344f4067e4e03b66608ac0e58bb
Hillel Arnold <helrond@hotmail.com>
2020-08-18 13:47:48 -0400

    add data to localStorage

--- src/components/PageRecord.js
+++ src/components/PageRecord.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from "./Button";
 
-const PageRecord = () => (
- <div>
-    <h1>Collection or Object Details and Context</h1>
-</div>
-)
+class PageRecord extends Component {
+  constructor(props) {
+    super(props)
+    this.exampleData = {
+      "uri": "/objects/wjhfxgo5jdzsysz72cbypcyhp3"
+    }
+  }
+  saveItem = item => {
+	  var existing = localStorage.getItem("savedItems");
+    existing = existing ? JSON.parse(existing) : {};
+    existing[item.uri] = {"saved": Date.now()}
+	  localStorage.setItem("savedItems", JSON.stringify(existing));
+  }
+  render() {
+    return (
+      <div>
+         <h1>Collection or Object Details and Context</h1>
+         <Button
+           label="Add example data"
+           iconAfter="add_circle_outline"
+           className="btn--gray btn--sm"
+           onClick={this.saveItem(this.exampleData)} />
+      </div>
+    )
+  }
+}
 
 export default PageRecord;
