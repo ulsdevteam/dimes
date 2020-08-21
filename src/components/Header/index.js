c8a2050237f3adeb6a923a11c7e0489361e92b6d
Hillel Arnold <helrond@hotmail.com>
2020-08-21 16:36:37 -0400

    incorporate nav dropdown into header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,14 +7,14 @@ class Header extends Component {
     return (
       <header className="header-secondary">
         <div className="wrapper">
-          <div className="container">
+          <div className="container--full-width">
             <div className="header-secondary__brand">
               <a href="/" className="header-secondary__title">dimes.rockarch.org</a>
               <p className="header-secondary__subtitle">The Online Collection and Catalog of Rockefeller Archive Center</p>
             </div>
             <Nav ariaLabel="Main">
-              <NavItem href="#" label="Sign in to RACcess" icon="arrow_right_alt" />
-              <NavItem href="list" label={`My List${this.props.myListCount > 0  ? ` (${this.props.myListCount})` : ""}`} icon="arrow_right_alt" />
+              <NavItem href="#" label="Sign in to RACcess" icon="east" />
+              <NavItem href="list" label={`My List${this.props.myListCount > 0  ? ` (${this.props.myListCount})` : ""}`} icon="east" />
             </Nav>
           </div>
         </div>
