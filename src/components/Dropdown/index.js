8568349983433efac9ac0d89fb0b0c16558a9621
Hillel Arnold <helrond@hotmail.com>
2020-08-21 16:35:31 -0400

    moving dropdowns into a single file

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,28 +1,39 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import useDropdownMenu from 'react-accessible-dropdown-menu-hook';
 import MaterialIcon from "../MaterialIcon";
 import "../Button/styles.scss";
 import "./styles.scss";
 
 const Dropdown = props => {
-  const [opts] = useState(props);
-  const { buttonProps, itemProps, isOpen } = useDropdownMenu(opts.items.length);
-  const listItems = opts.items.map((item, idx) =>
-        <a
-          key={idx}
-          id={`menu-item-${idx}`}
-          className={`btn btn--orange btn--dropdown ${opts.itemClassName}`}
-          onClick={item.onClick}
-          href={item.href}
-          {...itemProps[idx]}>{item.iconBefore && <MaterialIcon icon={item.iconBefore} />}{item.label}</a>
-      );
+  const [buttonClassName] = useState(props.buttonClassName);
+  const [className] = useState(props.className);
+  const [iconBefore] = useState(props.iconBefore);
+  const [iconBeforeOpen] = useState(props.iconBeforeOpen);
+  const [items] = useState(props.items);
+  const [itemClassName] = useState(props.itemClassName);
+  const [label] = useState(props.label);
+  const [listClassName] = useState(props.listClassName);
+  const { buttonProps, itemProps, isOpen } = useDropdownMenu(items.length);
+  const openIcon = iconBeforeOpen ? iconBeforeOpen : iconBefore
+  const listItems = items.map((item, idx) =>
+    <a
+      key={idx}
+      id={`menu-item-${idx}`}
+      className={`btn ${itemClassName}`}
+      onClick={item.onClick}
+      href={item.href}
+      {...itemProps[idx]}>{item.iconBefore && <MaterialIcon icon={item.iconBefore} />}{item.label}{item.iconAfter && <MaterialIcon icon={item.iconAfter} />}</a>
+  );
   return (
-    <div className={`dropdown ${opts.className}`}>
+    <div className={`dropdown ${className}`}>
       <button
-        className={`dropdown__button ${opts.buttonClassName}`}
-        {...buttonProps} >{opts.iconBefore && <MaterialIcon icon={opts.iconBefore} />}{opts.label}</button>
+        className={`dropdown__button ${buttonClassName} ${isOpen ? "open" : "closed" }`}
+        {...buttonProps} >
+          {isOpen ? (<MaterialIcon icon={openIcon} />) : (iconBefore && <MaterialIcon icon={iconBefore} />)}
+          {label}
+      </button>
       <div
-        className={`dropdown__list ${opts.listClassName} ${isOpen ? "open" : "closed" }`}
+        className={`dropdown__list ${listClassName} ${isOpen ? "open" : "closed" }`}
         role="menu" >
         {listItems}
       </div>
@@ -30,4 +41,78 @@ const Dropdown = props => {
   )
 }
 
-export default Dropdown;
+export class MyListDropdown extends Component {
+  render() {
+    return (
+      <Dropdown
+        label="Actions"
+        iconBefore="settings"
+        className="mylist__actions"
+        buttonClassName="btn btn--orange btn--md"
+        itemClassName="btn--orange btn--dropdown dropdown__item--orange"
+        listClassName="dropdown__list--orange"
+        items={
+          [
+            {
+              "label": "Schedule a Visit",
+              "iconBefore": "account_balance",
+              "onClick": null
+            },
+            {
+              "label": "Request in Reading Room",
+              "iconBefore": "local_library",
+              "onClick": null
+            },
+            {
+              "label": "Request Copies",
+              "iconBefore": "content_copy",
+              "onClick": null
+            },
+            {
+              "label": "Email List",
+              "iconBefore": "email",
+              "onClick": null
+            },
+            {
+              "label": "Download as .csv",
+              "iconBefore": "get_app",
+              "onClick": null
+            },
+            {
+              "label": "Remove All Items",
+              "iconBefore": "delete",
+              "onClick": this.props.removeAllItems
+            }
+          ]
+        } />
+    )
+  }
+}
+
+export class NavDropdown extends Component {
+  render() {
+    return (
+      <Dropdown
+        iconBefore="menu"
+        iconBeforeOpen="close"
+        className="nav-mobile hide-on-md-up"
+        buttonClassName="btn nav-mobile__btn"
+        itemClassName="btn--navy btn--mobile-dropdown dropdown__item--navy"
+        listClassName="dropdown__list--mobile dropdown__list--navy"
+        items={
+          [
+            {
+              "label": "Sign in to RACcess",
+              "iconAfter": "east",
+              "onClick": null
+            },
+            {
+              "label": "My List",
+              "iconAfter": "east",
+              "onClick": null
+            }
+          ]
+        } />
+    )
+  }
+}
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import MaterialIcon from "../MaterialIcon";
+import {NavDropdown} from "../Dropdown";
 import "./styles.scss";
 
 export class NavItem extends Component {
   render() {
     return (
-      <li className={`nav__item ${this.props.className}`}>
-        <a className={`nav__link ${this.props.className}`} href={this.props.href}>{this.props.label} {this.props.icon && <MaterialIcon icon={this.props.icon} />}</a>
+      <li className={`nav__item ${this.props.className ? this.props.className : ""}`}>
+        <a
+          className={`nav__link ${this.props.className ? this.props.className : ""}`}
+          href={this.props.href}>{this.props.label} {this.props.icon && <MaterialIcon icon={this.props.icon} />}</a>
       </li>
     )
   }
@@ -23,10 +26,11 @@ NavItem.propTypes = {
 export class Nav extends Component {
   render() {
     return (
-      <nav className={`nav ${this.props.className && this.props.className}`} aria-label={this.props.ariaLabel}>
-        <ul className="nav__list">
+      <nav className={`nav ${this.props.className ? this.props.className : ""}`} aria-label={this.props.ariaLabel}>
+        <ul className="nav__list show-on-md-up">
           {this.props.children}
         </ul>
+        <NavDropdown />
       </nav>
     )
   }
