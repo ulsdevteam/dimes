ef1d1cb1a98a3dd50eff8640b7bfd062d06ccc06
Hillel Arnold <helrond@hotmail.com>
2020-08-22 21:18:00 -0400

    adds smoke tests

---
+++ src/components/SavedItem/__tests__/SavedItem.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Modal from "react-modal";
+import {SavedItemList} from '../../SavedItem';
+
+const items = [
+  {
+    "title": "Cary Reich papers",
+    "uri": "/collections/qkxsgwnspzw9nh8htnoaiserc4/",
+    "items": [
+      {
+        "date": "undated",
+        "description": "Architect and business partnter of Wallace Harrison.",
+        "lastRequested": null,
+        "online": true,
+        "parent": "Interview Transcripts and Subject Files",
+        "parentRef": "/collections/zqx5prpdk44jz3o8ypv7mgrzf3",
+        "saved": 1598145061684,
+        "title": "Max Abramovitz Interview",
+        "uri": "/objects/wjhfxgo5jdzsysz72cbypcyhp3/"
+      },
+      {
+        "date": "undated",
+        "description": "",
+        "lastRequested": null,
+        "online": undefined,
+        "parent": "Interview Transcripts and Subject Files",
+        "parentRef": "/collections/zqx5prpdk44jz3o8ypv7mgrzf3",
+        "saved": 1598145062335,
+        "title": "Robert Alton, Interview 3",
+        "uri": "/objects/qckuiz9z5hb358esjsyuxqxfx4/"
+      }
+    ]
+  },
+  {
+    "title": "Rockefeller Brothers Fund records",
+    "uri": "/collections/rf8ifduv77hwzspm5eqow4ycw4/",
+    "items": [
+      {
+        "date": "1958-1960",
+        "description": "",
+        "lastRequested": null,
+        "online": undefined,
+        "parent": "Young Women's Christian Association - National Board",
+        "parentRef": "/collections/zg6zkb7q5oyfaeahgd7bcmdak3",
+        "saved": 1598145064266,
+        "title": "1958-1960",
+        "uri": "/objects/u8ihqyop5p4vxpq7oxg236f9y4/"
+      }
+    ]
+  }
+]
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  render(<SavedItemList items={items} isLoading={true} />, div);
+});
