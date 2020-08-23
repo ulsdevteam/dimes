3470fb37458812f9c45d6dddf57df223775293b3
Hillel Arnold <helrond@hotmail.com>
2020-08-22 21:18:35 -0400

    remove unused  imports

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import SocialIcons from "../SocialIcons";
 import "./styles.scss";
 
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "../../assets/homepage_logo.png"
 import "./styles.scss";
 
