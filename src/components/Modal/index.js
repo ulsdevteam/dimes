76c2aebe7c2d36a1ade7c408f112ce930bff8d21
Hillel Arnold <helrond@hotmail.com>
2020-08-31 16:48:23 -0400

    implement formik in reading room and duplication request modals

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,23 +3,24 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import axios from "axios";
 import PropTypes from "prop-types";
 import Modal from "react-modal";
-import Button from "../Button";
 import Captcha from "../Captcha";
-import {CheckBoxInput, DatePickerInput, EmailInput, SelectInput, SelectOption, TextInput, TextAreaInput} from "../Inputs";
 import MaterialIcon from "../MaterialIcon";
 import {ModalSavedItemList} from "../SavedItem";
 import "./styles.scss"
 
 
-const FormGroup = ({ component, error, label, type, name, rows}) => (
+const FormGroup = ({ children, component, error, helpText, label, type, name, rows}) => (
   <div className="form-group">
-    <label htmlFor={name}>{label}</label>
-    <Field type={type} name={name} component={component} rows={rows} />
-    { error && <ErrorMessage name={name} component="div" className="modal-form__error" />}
+    { type !== "checkbox" && <label htmlFor={name}>{label}</label> }
+    <Field type={type} name={name} component={component} rows={rows} children={children} />
+    { type === "checkbox" && <label htmlFor={name}>{label}</label> }
+    { helpText && <p className="help-text" aria-describedby={`desc-${name}`}>{helpText}</p> }
+    { error && <ErrorMessage name={name} component="div" className="modal-form__error" /> }
   </div>
 )
 
 FormGroup.propTypes = {
+  children: PropTypes.array,
   component: PropTypes.string,
   error: PropTypes.bool,
   label: PropTypes.string.isRequired,
@@ -158,15 +159,6 @@ EmailModal.propTypes = {
 }
 
 export class ReadingRoomRequestModal extends Component {
-  constructor(props)  {
-    super(props)
-    this.state  = {
-      "data": {},
-      "scheduledDate": new Date().toISOString().substring(0, 10),
-      "questions": "",
-      "notes": ""
-    }
-  }
   componentDidMount() {
     // TODO: what should we do if this request fails?
     axios
@@ -190,13 +182,6 @@ export class ReadingRoomRequestModal extends Component {
       }
     );
   }
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value});
-  }
-  handleCaptchaChange = value => {
-    // TODO: decide if we need this handler or not
-    console.log(value)
-  }
   render() {
     return (
       <MyListModal
@@ -204,56 +189,36 @@ export class ReadingRoomRequestModal extends Component {
         title="Request in Reading Room"
         isOpen={this.props.isOpen}
         toggleModal={this.props.toggleModal}
-
         list={this.props.list}
-        handleCaptchaChange={this.handleCaptchaChange}
-        submitError={this.props.error}
-        inputs={
-          <React.Fragment>
-            <DatePickerInput
-              id="scheduledDate"
-              name="scheduledDate"
-              className="modal-form__input"
-              label="Scheduled Date"
-              helpText="Enter the date of your research visit"
-              required={true}
-              value={this.state.scheduledDate}
-              handleChange={this.handleChange} />
-            <TextAreaInput
-              id="questions"
-              name="questions"
-              className="modal-form__input"
-              label="Special Requests/Questions for RAC staff"
-              helpText="255 characters maximum"
-              rows={5}
-              value={this.state.questions}
-              handleChange={this.handleChange} />
-            <TextAreaInput
-              id="notes"
-              name="notes"
-              className="modal-form__input"
-              label="Notes for Personal Reference"
-              helpText="255 characters maximum"
-              rows={5}
-              value={this.state.notes}
-              handleChange={this.handleChange} />
-          </React.Fragment>
-        }
-        buttons={
-          <React.Fragment>
-            <Button
-              className="btn--orange btn--sm"
-              type="submit"
-              value="submit"
-              label={`Request ${this.props.list.length ? (this.props.list.length) : ""} Items`}
-              handleClick={this.handleSubmit}
-              disabled={this.state.submitDisabled} />
-            <Button
-              className="btn--gray btn--sm"
-              type="reset"
-              label="Cancel"
-              handleClick={this.props.toggleModal} />
-          </React.Fragment>
+        form={
+          <Formik
+            initialValues={{scheduledDate: "", questions: "", notes: "", recaptcha: ""}}
+            validate={values => {
+              const errors = {};
+              if (!values.scheduledDate) {
+                errors.scheduledDate = 'This field is required.';
+              }
+              if (!values.recaptcha) {
+                errors.recaptcha = 'This field is required.';
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              this.handleSubmit(values);
+              setSubmitting(false);
+            }}
+          >
+          {({ isSubmitting, setFieldValue }) => (
+            <Form>
+              <FormGroup label="Scheduled Date *" name="scheduledDate" type="date" error={true} helpText="Enter the date of your research visit" />
+              <FormGroup label="Special Requests/Questions for RAC staff" helpText="255 characters maximum" name="questions" component="textarea" rows={5} />
+              <FormGroup label="Notes for Personal Reference" helpText="255 characters maximum" name="notes" component="textarea" rows={5} />
+              <Field component={Captcha} name="recaptcha" className="modal-form__captcha" handleCaptchaChange={(response) => setFieldValue("recaptcha", response)} />
+              <ErrorMessage name="recaptcha" component="div" className="modal-form__error" />
+              <FormButtons submitText={`Request ${this.props.list.length ? (this.props.list.length) : ""} Items`} toggleModal={this.props.toggleModal} isSubmitting={isSubmitting} />
+            </Form>
+          )}
+          </Formik>
         }
       />
     )
@@ -306,13 +271,6 @@ export class DuplicationRequestModal extends Component {
       }
     );
   }
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value});
-  }
-  handleCaptchaChange = value => {
-    // TODO: decide if we need this handler or not
-    console.log(value)
-  }
   render() {
     return (
       <MyListModal
@@ -321,75 +279,55 @@ export class DuplicationRequestModal extends Component {
         isOpen={this.props.isOpen}
         toggleModal={this.props.toggleModal}
         list={this.props.list}
-        handleCaptchaChange={this.handleCaptchaChange}
-        submitError={this.props.error}
-        inputs={
-          <React.Fragment>
-            <SelectInput
-              id="format"
-              name="format"
-              className="modal-form__input"
-              label="Format"
-              required={true}
-              value={this.state.format}
-              handleChange={this.handleChange} >
-                <SelectOption label="JPEG" />
-                <SelectOption label="PDF" />
-                <SelectOption label="Photocopy" />
-                <SelectOption label="TIFF" />
-            </SelectInput>
-            <TextAreaInput
-              id="description"
-              name="description"
-              className="modal-form__input"
-              label="Description of Materials"
-              helpText="Please describe the materials you want reproduced"
-              rows={5}
-              value={this.state.description}
-              handleChange={this.handleChange} />
-            <TextAreaInput
-              id="questions"
-              name="questions"
-              className="modal-form__input"
-              label="Special Requests/Questions for RAC staff"
-              helpText="255 characters maximum"
-              rows={5}
-              value={this.state.questions}
-              handleChange={this.handleChange} />
-            <TextAreaInput
-              id="notes"
-              name="notes"
-              className="modal-form__input"
-              label="Notes for Personal Reference"
-              helpText="255 characters maximum"
-              rows={5}
-              value={this.state.notes}
-              handleChange={this.handleChange} />
-            <CheckBoxInput
-              id="costs"
-              name="costs"
-              className="modal-form__input"
-              label={<>I agree to pay the duplication costs for this request. See our <a href="https://rockarch.org/collections/access-and-request-materials/#duplication-services-and-fee-schedule">fee schedule</a></>}
-              required={true}
-              checked={this.state.costs}
-              handleChange={this.handleChange} />
-          </React.Fragment>
-        }
-        buttons={
-          <React.Fragment>
-            <Button
-              className="btn--orange btn--sm"
-              type="submit"
-              value="submit"
-              label={`Request ${this.props.list.length ? (this.props.list.length) : ""} Items`}
-              handleClick={this.handleSubmit}
-              disabled={this.state.submitDisabled} />
-            <Button
-              className="btn--gray btn--sm"
-              type="reset"
-              label="Cancel"
-              handleClick={this.props.toggleModal} />
-          </React.Fragment>
+        form={
+          <Formik
+            initialValues={{format: "", description: "Entire folder", questions: "", notes: "", costs: false, recaptcha: ""}}
+            validate={values => {
+              const errors = {};
+              if (!values.format) {
+                errors.format = 'This field is required.';
+              }
+              if (!values.recaptcha) {
+                errors.recaptcha = 'This field is required.';
+              }
+              if (!values.costs) {
+                errors.costs = "We cannot process your request unless you agree to pay the costs of reproduction.";
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              this.handleSubmit(values);
+              setSubmitting(false);
+            }}
+          >
+          {({ isSubmitting, setFieldValue }) => (
+            <Form>
+              <FormGroup
+                label="Format *"
+                name="format"
+                component="select"
+                error={true}
+                children={[
+                  <option value="">Select a format</option>,
+                  <option value="JPEG">JPEG</option>,
+                  <option value="PDF">PDF</option>,
+                  <option value="Photocopy">Photocopy</option>,
+                  <option value="TIFF">TIFF</option>
+                ]}/>
+              <FormGroup label="Description of Materials" helpText="Please describe the materials you want reproduced" name="description" component="textarea" rows={5} />
+              <FormGroup label="Special Requests/Questions for RAC staff" helpText="255 characters maximum" name="questions" component="textarea" rows={5} />
+              <FormGroup label="Notes for Personal Reference" helpText="255 characters maximum" name="notes" component="textarea" rows={5} />
+              <FormGroup
+                label={<>I agree to pay the duplication costs for this request. See our <a href="https://rockarch.org/collections/access-and-request-materials/#duplication-services-and-fee-schedule">fee schedule</a></>}
+                name="costs"
+                type="checkbox"
+                error={true} />
+              <Field component={Captcha} name="recaptcha" className="modal-form__captcha" handleCaptchaChange={(response) => setFieldValue("recaptcha", response)} />
+              <ErrorMessage name="recaptcha" component="div" className="modal-form__error" />
+              <FormButtons submitText={`Request ${this.props.list.length ? (this.props.list.length) : ""} Items`} toggleModal={this.props.toggleModal} isSubmitting={isSubmitting} />
+            </Form>
+          )}
+          </Formik>
         }
       />
     )
