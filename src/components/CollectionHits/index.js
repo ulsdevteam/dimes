fd2d951fa2ab448c6b4f89906e2b56983e906784
Hillel Arnold <helrond@hotmail.com>
2020-09-13 17:34:25 -0400

    style search hits modal

--- src/components/CollectionHits/index.js
+++ src/components/CollectionHits/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { HitCount } from "../Tile";
+import "./styles.scss"
 
 const CollectionHitsInfo = ({collection}) => (
   <div className="collection-hits__info">
-    <h4 className="collection-hits__title">Collection Name:</h4>
+    <h3 className="collection-hits__title">Collection Name:</h3>
     <a className="collection-hits__link" href={collection.uri}>{collection.title}</a>
-    <h4 className="collection-hits__title">{collection.creators.length > 1 ? "Creators:" : "Creator:"}</h4>
-    {collection.creators.map(c => <a href={c.uri}>{c.title}</a>)}
-    <h4 className="collection-hits__title">Dates:</h4>
+    <h3 className="collection-hits__title">{collection.creators.length > 1 ? "Creators:" : "Creator:"}</h3>
+    {collection.creators.map(c => <a className="collection-hits__link" href={c.uri}>{c.title}</a>)}
+    <h3 className="collection-hits__title">Dates:</h3>
     <p className="collection-hits__text">{collection.dates.map(d => d.expression).join(", ")}</p>
   </div>
 )
@@ -16,7 +17,7 @@ const CollectionHits = ({ collection }) => {
   const collectionChildHits = collection.children.map((child, idx) =>
     <div className="collection-child" key={idx}>
       {child.hit_count ? (<HitCount hit_count={child.hit_count} />) : null}
-      <h3 className="collection-child__title">{child.title}</h3>
+      <a href={child.uri} className="collection-child__title">{child.title}</a>
     </div>
   )
   return (
