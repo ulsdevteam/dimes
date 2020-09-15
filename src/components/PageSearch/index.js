d22168788adf37b2e0def820a7d21812113c8929
Hillel Arnold <helrond@hotmail.com>
2020-09-15 16:56:11 -0400

    restructuring search page HTML

--- src/components/PageSearch/index.js
+++ src/components/PageSearch/index.js
@@ -17,7 +17,7 @@ class PageSearch extends Component {
       inProgress: false,
       items: [],
       offset: 0,
-      params: this.parseParams(this.props.location.search),
+      params: {},
       pageCount: 0,
       pageSize: 40,
       startItem: 0,
@@ -28,18 +28,21 @@ class PageSearch extends Component {
     };
   };
   componentDidMount() {
-    this.executeSearch(this.state.params)
+    let params = this.parseParams(this.props.location.search)
+    params.limit = this.state.pageSize
+    this.executeSearch(params)
   };
   startItem = (results, offset) => {
     var startItem = this.state.startItem;
-    if (offset) startItem = offset;
-    if (results.count) startItem = 1
+    if (results.count) startItem = 1;
+    if (offset > 0) startItem = offset;
     return startItem;
   }
   endItem = (results, offset) => {
     var endItem = results.count
     if (results.count > this.state.pageSize) endItem = this.state.pageSize;
-    if (offset) endItem = offset + this.state.pageSize;
+    if (offset) endItem = Math.ceil(Number(offset) + Number(this.state.pageSize));
+    if (endItem > results.count) endItem = results.count
     return endItem;
   }
   excecuteFacetsSearch = params =>  {
@@ -120,39 +123,41 @@ class PageSearch extends Component {
               category={this.state.params.category}
              />
           </div>
-          <div className="search-results">
-            <h1 className="search__title">{`Search Results ${this.state.params.query && `for “${this.state.params.query}”` }`}</h1>
-            <p className="results__summary">
-              {`${this.state.startItem === this.state.endItem ?
-                this.state.startItem :
-                `${this.state.startItem}-${this.state.endItem}`} of ${this.state.resultsCount} results`}
-            </p>
-            <div className="search__controls">
-              <Button
-                handleClick={() => this.toggleFacetModal()}
-                label="Filters"
-                iconBefore="filter_alt"
-                className="btn--filter" />
-              <div className="select__sort--wrapper">
-                <SelectInput
-                  className="hide-label select__sort"
-                  handleChange={this.handleSortChange}
-                  id="sort"
-                  label="Sort search results"
-                  defaultValue={this.state.params.sort} >
-                    <SelectOption value="" label="Sort by relevance" />
-                    <SelectOption value="title" label="Sort by title" />
-                    <SelectOption value="creator" label="Sort by creator name" />
-                </SelectInput>
+          <div className="results">
+            <h1 className="results__title">{`Search Results ${this.state.params.query && `for “${this.state.params.query}”` }`}</h1>
+            <div className="results__header">
+              <p className="results__summary">
+                {`${this.state.startItem === this.state.endItem ?
+                  this.state.startItem :
+                  `${this.state.startItem}-${this.state.endItem}`} of ${this.state.resultsCount} results`}
+              </p>
+              <div className="results__controls">
+                <Button
+                  handleClick={() => this.toggleFacetModal()}
+                  label="Filters"
+                  iconBefore="filter_alt"
+                  className="btn--filter" />
+                <div className="select__sort--wrapper">
+                  <SelectInput
+                    className="hide-label select__sort"
+                    handleChange={this.handleSortChange}
+                    id="sort"
+                    label="Sort search results"
+                    defaultValue={this.state.params.sort} >
+                      <SelectOption value="" label="Sort by relevance" />
+                      <SelectOption value="title" label="Sort by title" />
+                      <SelectOption value="creator" label="Sort by creator name" />
+                  </SelectInput>
+                </div>
               </div>
-            </div>
-            <div className="search__pagination">
-              <SearchPagination
-                offset={this.state.offset}
-                pageSize={this.state.pageSize}
-                pageCount={this.state.pageCount}
-                handlePageClick={this.handlePageClick}
-              />
+              <div className="results__pagination">
+                <SearchPagination
+                  offset={this.state.offset}
+                  pageSize={this.state.pageSize}
+                  pageCount={this.state.pageCount}
+                  handlePageClick={this.handlePageClick}
+                />
+              </div>
             </div>
             { this.state.inProgress ? (<p>Searching</p>) : (<TileList items={this.state.items} />)}
           </div>
