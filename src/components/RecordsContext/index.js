30c8c9ea0562b35b6d84a5f134c1ed80374bae80
Hillel Arnold <helrond@hotmail.com>
2020-09-28 12:15:12 -0400

    call RecordsContext List recursively

--- src/components/RecordsContext/index.js
+++ src/components/RecordsContext/index.js
@@ -10,100 +10,123 @@ import "./styles.scss";
 class RecordsChild extends Component {
   constructor(props) {
     super(props)
-    this.item = props.item;
     this.state = {
-      isActive: false,
       isChildrenLoading: false,
-      children: [],
+      itemChildren: [],
     }
   }
 
   handleClick = item => {
-    const prevActiveState = this.state.isActive;
-    this.setState({ isActive: !this.state.isActive });
-    if (!prevActiveState) {
-      this.props.toggleIsLoading();
+    const {params, parent, setActiveRecords, setParent, toggleActiveChild, toggleIsLoading} = this.props
+    const isActive = toggleActiveChild(item);
+    if (isActive) {
+      toggleIsLoading();
       // Only fetch children if this is a collection and we haven't already fetched them.
-      if (item.type === "collection" && !this.state.children.length) {
+      if (item.type === "collection" && !this.state.itemChildren.length) {
         this.setState({ isChildrenLoading: true })
       }
       axios
-        .get(`${process.env.REACT_APP_ARGO_BASEURL}/${item.uri}?${queryString.stringify(this.props.params)}`)
+        .get(`${process.env.REACT_APP_ARGO_BASEURL}/${item.uri}?${queryString.stringify(params)}`)
         .then(res => {
-          this.setState({ children: res.data.children })
+          this.setState({ itemChildren: res.data.children })
           this.setState({ isChildrenLoading: false })
-          this.props.setActiveRecords(res.data)
-          this.props.toggleIsLoading();
+          setParent(res.data);
+          setActiveRecords(res.data);
+          toggleIsLoading();
         })
         .catch(e => console.log(e))
     } else {
-      this.props.setActiveRecords(this.props.collection);
+      setActiveRecords(parent);
     }
   }
 
-  itemChildList = (parent, children, props) => {
-    return children && children.map((item, index) => (
-      <RecordsChild
-        key={item.uri}
-        item={item}
-        params={props.params}
-        setActiveRecords={props.setActiveRecords}
-        toggleIsLoading={props.toggleIsLoading} />
+  render() {
+    const {item, params, setActiveRecords, setParent, toggleIsLoading} = this.props;
+    return (
+      <li>
+        <div className={`child__list-item child__list-item--${item.type} ${item.isActive ? "active" : ""}`} >
+          <button className={`child__title child__title--${item.type}`} onClick={() => this.handleClick(item)}>{item.title}</button>
+          <p className="child__text">{item.dates}</p>
+          <p className="child__text child__description">{item.description}</p>
+          {this.state.hit_count ? (<HitCount className="hit-count--records-context" hitCount={item.hit_count} />) : null}
+        </div>
+        {this.state.isChildrenLoading ? <RecordsChildSkeleton/> : null}
+        {(this.state.itemChildren && this.state.itemChildren.length) ?
+          (<RecordsContextList
+            children={this.state.itemChildren}
+            className={!item.isActive && "hidden"}
+            parent={item}
+            params={params}
+            setActiveRecords={setActiveRecords}
+            setParent={setParent}
+            toggleIsLoading={toggleIsLoading} />) :
+          (null)}
+      </li>
+    )
+  }
+}
+
+class RecordsContextList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      children: props.children
+    }
+  }
+
+  toggleActiveChild = item => {
+    const toggled = !item.isActive
+    const updated = this.state.children.map(child => (
+      { ...child, isActive: child.uri === item.uri ? toggled : false }
+    ))
+    this.setState({children: updated})
+    return toggled;
+  }
+
+  setParent = parent => {
+    this.setState({ parent: parent })
+  }
+
+  childList = children => {
+    const {parent, params, setActiveRecords, toggleIsLoading} = this.props;
+    return (
+      children.map(child => (
+        <RecordsChild
+          key={child.uri}
+          parent={parent}
+          item={child}
+          params={params}
+          toggleActiveChild={this.toggleActiveChild}
+          setActiveRecords={setActiveRecords}
+          setParent={this.setParent}
+          toggleIsLoading={toggleIsLoading} />
+        )
       )
     )
   }
 
   render() {
     return (
-      <li>
-        <div className={`child__list-item child__list-item--${this.item.type} ${this.state.isActive ? "active" : ""}`} >
-          <button className={`child__title child__title--${this.item.type}`} onClick={() => this.handleClick(this.item)}>{this.item.title}</button>
-          <p className="child__text">{this.item.dates}</p>
-          <p className="child__text child__description">{this.item.description}</p>
-          {this.state.hit_count ? (<HitCount className="hit-count--records-context" hitCount={this.item.hit_count} />) : null}
-        </div>
-        {this.state.children ?
-          (this.state.isChildrenLoading ?
-            (<RecordsChildSkeleton/>) :
-            (<ul className={`child__list ${this.state.isActive ? "" : "hidden"}`}>{this.itemChildList(this.item, this.state.children, this.props)}</ul>)) :
-          (null)}
-      </li>
-  )
-}
-}
-
-const RecordsContextList = ({ collection, params, setActiveRecords, toggleIsLoading}) => {
-
-  const childList = collection.children.map(child => (
-    <RecordsChild
-      key={child.uri}
-      collection={collection}
-      item={child}
-      params={params}
-      setActiveRecords={setActiveRecords}
-      toggleIsLoading={toggleIsLoading} />
+      <ul className={`child__list--full-width ${this.props.className && this.props.className}`}>
+        {this.childList(this.state.children)}
+      </ul>
     )
-  )
-
-  return (
-    <ul className="child__list--full-width">
-      {childList}
-    </ul>
-  )
+  }
 }
 
-const RecordsContext = ({ params, records, setActiveRecords, toggleIsLoading }) => {
-  const collection = (records.ancestors && records.ancestors.length) ? records.ancestors.slice(0)[0] : records;
+const RecordsContext = ({ params, parent, setActiveRecords, toggleIsLoading }) => {
+  const collection = (parent.ancestors && parent.ancestors.length) ? parent.ancestors.slice(0)[0] : parent;
 
   return (
-  records.children ?
+  parent.children ?
   (<div className="records__context">
     <h2 className="context__title">Collection Context</h2>
     <h3 className="collection__title">{collection.title}</h3>
     <p className="collection__date">{Array.isArray(collection.dates) ? dateString(collection.dates) : collection.dates }</p>
     <p className="collection__text collection__description">{collection.description}</p>
     <RecordsContextList
-      collection={records}
+      children={parent.children}
+      parent={parent}
       params={params}
       setActiveRecords={setActiveRecords}
       toggleIsLoading={toggleIsLoading} />
