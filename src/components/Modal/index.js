d324d7d37a2a6527ac38e78a6288311f882d07d8
Hillel Arnold <helrond@hotmail.com>
2020-10-07 10:56:06 -0400

    Better string handling
    Co-authored-by: Hannah Sistrunk <hsistrunk@rockarch.org>

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -190,7 +190,7 @@ export const ReadingRoomRequestModal = props => (
             component="div"
             className="modal-form__error" />
           <FormButtons
-            submitText={`Request ${props.submitList.length ? (props.submitList.length) : "0"} ${props.submitList.length > 1 ? "Items" : "Item"}`}
+            submitText={`Request ${props.submitList.length ? (props.submitList.length) : "0"} ${props.submitList.length !== 1 ? "Items" : "Item"}`}
             toggleModal={props.toggleModal}
             isSubmitting={isSubmitting} />
           <FocusError />
