56b7caa3b091775f545f1d9ff3a315364157607c
Hillel Arnold <helrond@hotmail.com>
2020-10-15 13:06:40 -0400

    change hardcoded manifest uri

--- src/components/PageDigitalObject/index.js
+++ src/components/PageDigitalObject/index.js
@@ -3,7 +3,7 @@ import Viewer from "../Viewer"
 
 
 const PageDigitalObject = props => (
- <Viewer manifest="http://wellcomelibrary.org/iiif/b18035723/manifest" />
+ <Viewer manifest="https://raciif-dev.s3.amazonaws.com/manifests/f430dec48d764f218c2147a846e28704" />
 )
 
 export default PageDigitalObject;
