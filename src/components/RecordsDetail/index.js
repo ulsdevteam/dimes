aa9905a3b7c748a99bcdafb858245289aac7bcdd
Hillel Arnold <helrond@hotmail.com>
2020-10-18 12:23:52 -0400

    show physdesc info in formats

--- src/components/RecordsDetail/index.js
+++ src/components/RecordsDetail/index.js
@@ -43,14 +43,14 @@ const PanelFormatSection = ({ formats, notes }) => {
   const displayFormats = formats.filter(f => (
     f !== "documents"
   ))
-  const materialSpecificText = noteText(notes, "materialspec")
+  const materialSpecificText = (noteText(notes, "physdesc") || "").concat("\n", noteText(notes, "materialspec"))
   return (
     displayFormats.length ? (
       <div className="panel__section">
         <h3 className="panel__heading">Formats</h3>
         <ul className="panel__list--unstyled">
           {materialSpecificText ?
-            (<li>{materialSpecificText}</li>) :
+            (<li className="panel__text">{materialSpecificText}</li>) :
             (displayFormats.map((format, index) => (
               <li key={index} className="panel__text">{format}</li>))
             )
