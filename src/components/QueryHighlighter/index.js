41e46e00e2476af2ea2a364cf0da7ae43e9c6c9c
Hillel Arnold <helrond@hotmail.com>
2020-10-20 18:38:56 -0400

    correctly import styles

--- src/components/QueryHighlighter/index.js
+++ src/components/QueryHighlighter/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Highlighter from "react-highlight-words";
+import "./styles.scss";
 
 
 const QueryHighlighter = ({ query, text}) => {
