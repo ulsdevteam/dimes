ed8a3fab91db10af5d2a10337dbd70a55d2ed255
Hillel Arnold <helrond@hotmail.com>
2020-10-23 16:28:59 -0400

    styles Mirador

--- src/components/PageDigitalObject/index.js
+++ src/components/PageDigitalObject/index.js
@@ -1,23 +1,65 @@
 import React from 'react';
 import Viewer from "../Viewer"
 
-
 const configs = {
   id: "root",
-  selectedTheme: "light",
+  selectedTheme: "rac",
+  themes: {
+    rac:{
+      palette: {
+        type: 'dark',
+        primary: {
+          main: '#ffffff',
+        },
+        secondary: {
+          main: '#2F2F2F',
+        },
+        shades: {
+          main: '#000000',
+          dark: '#2F2F2F',
+          light: '#000000',
+        },
+        background: {
+          paper: '#000000',
+        },
+      },
+      typography:{
+        fontFamily: ['Lato', 'sans-serif'],
+        body1: {
+          color: '#ffffff',
+        },
+        h3: {
+          color: '#ffffff',
+        },
+        h4: {
+          color: '#ffffff',
+        },
+        overline: {
+          color: '#ffffff',
+        }
+      },
+      overrides: {
+        MuiDrawer: {
+          background: 'blue'
+        }
+      }
+    }
+  },
+  thumbnailNavigation: {
+    defaultPosition: 'off',
+  },
   window: {
     allowClose: false,
     allowMaximize: false,
     allowTopMenuButton: false,
     defaultSideBarPanel: 'canvas',
     defaultView: 'single',
-    sideBarOpenByDefault: true,
-  },
-  thumbnailNavigation: {
-    defaultPosition: 'off',
+    hideWindowTitle: false,
+    sideBarOpen: true,
   },
   workspace: {
     type: 'none',
+    showZoomControls: true,
   },
   workspaceControlPanel: {
     enabled: false,
