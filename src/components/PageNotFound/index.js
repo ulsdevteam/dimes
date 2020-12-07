5a4322aa873cdeac2eacd110a7014aa31cc2d9c4
Hillel Arnold <helrond@hotmail.com>
2020-12-07 16:38:24 -0500

    add title to 404 page

--- src/components/PageNotFound/index.js
+++ src/components/PageNotFound/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 import MaterialIcon from "../MaterialIcon";
 import "./styles.scss";
 
 const PageNotFound = () => (
- <div className="not-found">
-    <span className="not-found__icon"><MaterialIcon icon="help_outline" /></span>
-    <h1 className="not-found__header">Sorry, the requested page was not found!</h1>
-    <p className="not-found__text">Try <a href="/">a search</a> to find what you're looking for.</p>
-</div>
+  <>
+    <Helmet>
+      <title>Page Not Found</title>
+    </Helmet>
+    <div className="not-found">
+      <span className="not-found__icon"><MaterialIcon icon="help_outline" /></span>
+      <h1 className="not-found__header">Sorry, the requested page was not found!</h1>
+      <p className="not-found__text">Try <a href="/">a search</a> to find what you're looking for.</p>
+    </div>
+  </>
 )
 
 export default PageNotFound;
