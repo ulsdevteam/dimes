27f6492a96da7aa26698569fe784766a1a3df3bc
Hillel Arnold <helrond@hotmail.com>
2020-12-23 17:19:02 -0500

    copy deploy scripts

--- deploy_scripts/make_zip.sh
+++ deploy_scripts/make_zip.sh
@@ -8,6 +8,7 @@ if [ ! -f $ZIP_DIR/$ZIP_NAME ]; then
 
   # copy appspec file
   cp appspec.yml build/
+  cp -r deploy_scripts/ build/
   # create zip file from build dir
   zip -r $ZIP_NAME build
   mkdir -p $ZIP_DIR
