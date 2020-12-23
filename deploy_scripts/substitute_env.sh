f72f1e23995e8dcfb6f5b8c51dab9f824966bb10
Hillel Arnold <helrond@hotmail.com>
2020-12-23 09:40:27 -0500

    prepping for ec2 deployment

---
+++ deploy_scripts/substitute_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+for TEMPLATE in appspec.yml.deploy \
+  deploy_scripts/*.deploy
+do
+  if [[ -f "$TEMPLATE" ]]; then
+    echo "Replacing variables in $TEMPLATE"
+    envsubst < "$TEMPLATE" > `echo "$TEMPLATE" | sed -e 's/\(\.deploy\)*$//g'`
+    rm $TEMPLATE
+  fi
+done
