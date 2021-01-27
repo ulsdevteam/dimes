383f940a55830d30851d3b23b72406c164a04711
Hillel Arnold <helrond@hotmail.com>
2021-01-27 10:30:00 -0500

    improve tests

--- src/components/Dropdown/__tests__/Dropdown.test.js
+++ src/components/Dropdown/__tests__/Dropdown.test.js
@@ -1,13 +1,53 @@
 import React from "react";
-import { render } from "react-dom";
-import {MyListDropdown, NavDropdown} from "..";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils"
+import { MyListDropdown, NavDropdown } from "..";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<MyListDropdown />, div);
+  act(() => {
+    render(<MyListDropdown
+      downloadCsv={jest.fn()}
+      duplicationRequest={jest.fn()}
+      emailList={jest.fn()}
+      readingRoomRequest={jest.fn()}
+      removeAllItems={jest.fn()} />, container);
+  })
+
+  const dropdown = document.querySelector(".dropdown")
+  expect(dropdown.className).toBe("dropdown mylist__actions")
+  const button = document.querySelector(".dropdown > button")
+  expect(button.className).toBe("btn btn--orange btn--md closed")
+  const list = document.querySelector(".dropdown > div")
+  expect(list.className).toBe("dropdown__list dropdown__list--orange dropdown__list--slide-down closed")
+
 });
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<NavDropdown />, div);
+
+  act(() => {
+    render(<NavDropdown />, container);
+  })
+
+  const dropdown = document.querySelector(".dropdown")
+  expect(dropdown.className).toBe("dropdown hide-on-lg-up")
+  const button = document.querySelector(".dropdown > button")
+  expect(button.className).toBe("btn nav-mobile__btn closed")
+  const list = document.querySelector(".dropdown > div")
+  expect(list.className).toBe("dropdown__list dropdown__list--mobile dropdown__list--navy dropdown__list--slide-left closed")
+
 });
