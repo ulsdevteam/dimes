e5b9930ecdbab59663a322c06d9715c9927ed200
Hillel Arnold <helrond@hotmail.com>
2021-01-27 09:08:33 -0500

    improve badge tests

--- src/components/Badge/__tests__/Badge.test.js
+++ src/components/Badge/__tests__/Badge.test.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { act } from "react-dom/test-utils";
 import Badge from '..';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Badge label="This is a badge" />, div);
+  document.body.appendChild(div);
+
+  act(() => {
+    ReactDOM.render(<Badge label="This is a badge" />, div);
+  })
+
+  const badge = document.querySelector("span");
+  expect(badge.textContent).toContain("This is a badge")
+
+  act(() => {
+    ReactDOM.render(<Badge className="foo" icon="bar" label="This is a badge" />, div);
+  })
+
+  expect(badge.className).toBe("foo")
+  expect(badge.textContent).toContain("bar")
+  expect(badge.textContent).toContain("This is a badge")
+
 });
