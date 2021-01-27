30a9385e30fb40779f4bdadd2c887774b36aee04
Hillel Arnold <helrond@hotmail.com>
2021-01-27 09:00:27 -0500

    converts to function component, cleans up

--- src/components/AgentAttribute/__tests__/AgentAttribute.test.js
+++ src/components/AgentAttribute/__tests__/AgentAttribute.test.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import AgentAttributeList from '..';
 
 const items = [
-  {"label": "foo", "value": "foobar"},
-  {"label": "bar", "value": "barfoo"}
+  {"label": "foo", "value": "foobar", "note": true},
+  {"label": "bar", "value": "barfoo", "note": false}
 ];
 
 it('renders without crashing', () => {
