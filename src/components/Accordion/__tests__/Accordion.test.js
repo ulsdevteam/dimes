7c3482f5dbb9d991a9d3673352b3b24adbaf24e6
Hillel Arnold <helrond@hotmail.com>
2021-01-27 08:53:55 -0500

    add test for mouse click;

--- src/components/Accordion/__tests__/Accordion.test.js
+++ src/components/Accordion/__tests__/Accordion.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { act, Simulate } from "react-dom/test-utils";
 import {
     Accordion,
     AccordionItem,
@@ -26,6 +27,26 @@ it('renders without crashing', () => {
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<AccordionItemButton />, div);
+});
+
+it('handles clicks', () => {
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  const onClick = jest.fn();
+  const setIsExpanded = jest.fn();
+
+  act(() => {
+    ReactDOM.render(<AccordionItemButton setIsExpanded={setIsExpanded} onClick={onClick} />, div);
+  })
+
+  const button = document.querySelector("[data-accordion-component=AccordionItemButton]");
+
+  act(() => {
+    button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  expect(onClick).toHaveBeenCalledTimes(1)
+  expect(setIsExpanded).toHaveBeenCalledTimes(1)
 });
 
 it('renders without crashing', () => {
