71e8ff05fb2a3da464ccb981800575fc27e021c5
Hillel Arnold <helrond@hotmail.com>
2021-01-27 09:34:19 -0500

    better imports

--- src/components/Badge/__tests__/Badge.test.js
+++ src/components/Badge/__tests__/Badge.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { act } from "react-dom/test-utils";
 import Badge from '..';
 
@@ -8,14 +8,14 @@ it('renders without crashing', () => {
   document.body.appendChild(div);
 
   act(() => {
-    ReactDOM.render(<Badge label="This is a badge" />, div);
+    render(<Badge label="This is a badge" />, div);
   })
 
   const badge = document.querySelector("span");
   expect(badge.textContent).toContain("This is a badge")
 
   act(() => {
-    ReactDOM.render(<Badge className="foo" icon="bar" label="This is a badge" />, div);
+    render(<Badge className="foo" icon="bar" label="This is a badge" />, div);
   })
 
   expect(badge.className).toBe("foo")
