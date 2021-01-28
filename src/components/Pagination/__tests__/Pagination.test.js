7d171bcf3fe1a448adeeb73ea754032fde8af651
Hillel Arnold <helrond@hotmail.com>
2021-01-28 14:07:20 -0500

    improve formatting

--- src/components/Pagination/__tests__/Pagination.test.js
+++ src/components/Pagination/__tests__/Pagination.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {render} from 'react-dom';
-import Modal from "react-modal";
-import {SearchPagination} from '..';
+import { render } from 'react-dom';
+import { SearchPagination } from '..';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
