6e095307816e2266e52002dc421f6903f3b8c556
Hillel Arnold <helrond@hotmail.com>
2021-03-29 14:57:39 -0400

    updates styles for home and interior pages

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import classnames from 'classnames'
 import Button from '../Button'
 import PropTypes from 'prop-types'
 import { CheckBoxInput, SelectInput, TextInput } from '../Inputs'
@@ -8,6 +9,7 @@ const SearchForm = props => {
   var [category, setCategory] = useState(props.category || '')
   var [online, setOnline] = useState(props.online)
   var [query, setQuery] = useState(props.query)
+  const isHomePage = props.className === 'search-form--home'
 
   /** Sets the search category, query and online checkbox */
   useEffect(() => {
@@ -24,47 +26,51 @@ const SearchForm = props => {
   ]
 
   return (
-    <form role='search' action='/search' method='get'>
-      <div className={props.className}>
-        <div className='input-group__search'>
-          <TextInput
-            className='hide-label input__search'
-            label='Enter a search term'
-            id='query'
-            placeholder='Search...'
-            size={60}
-            value={query || ''}
-            handleChange={e => setQuery(e.target.value)}
-            type='search'
-            required
-          />
-          <Button
-            className='btn--search'
-            type='submit'
-            label='Search'
-            iconAfter='search'
-          />
-        </div>
-        <div className='input-group__search-controls'>
-          <SelectInput
-            className='select__search'
-            hideLabel
-            iconAfter='expand_more'
-            id='category'
-            label='Choose a search category'
-            name='category'
-            onChange={({selectedItem}) => setCategory(selectedItem.value)}
-            options={selectOptions}
-            selectedItem={category || ''}
-          />
-          <CheckBoxInput
-            id='online'
-            name='online'
-            className='checkbox--blue checkbox--online input--outline'
-            checked={online}
-            handleChange={e => setOnline(e.target.checked)}
-            label='Show only results with digital matches'
-          />
+    <form className='form--search' role='search' action='/search' method='get'>
+      <div class="wrapper">
+        <div className={props.className}>
+          <div className={classnames('input-group__search', { 'input-group__search-results': !isHomePage })}>
+            <TextInput
+              className={classnames('hide-label', 'input__search', { 'input__search-results': !isHomePage })}
+              label='Enter a search term'
+              id='query'
+              placeholder='Search...'
+              size={60}
+              value={query || ''}
+              handleChange={e => setQuery(e.target.value)}
+              type='search'
+              required
+            />
+            <Button
+              className={classnames({ 'btn--search': isHomePage, 'btn--search-results': !isHomePage })}
+              type='submit'
+              label={isHomePage ? 'Search' : null}
+              iconAfter='search'
+            />
+          </div>
+          <div className={classnames(
+              'input-group__search-controls',
+              { 'input-group__search-controls-results' : !isHomePage })}>
+            <SelectInput
+              className='select__search'
+              hideLabel
+              iconAfter='expand_more'
+              id='category'
+              label='Choose a search category'
+              name='category'
+              onChange={({selectedItem}) => setCategory(selectedItem.value)}
+              options={selectOptions}
+              selectedItem={category || ''}
+            />
+            <CheckBoxInput
+              id='online'
+              name='online'
+              className='checkbox--blue checkbox--online input--outline'
+              checked={online}
+              handleChange={e => setOnline(e.target.checked)}
+              label='Show only results with digital matches'
+            />
+          </div>
         </div>
       </div>
     </form>)
