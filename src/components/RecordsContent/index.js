1b66fbdf22cd6e33dcb282da14a12eac2589e165
Hillel Arnold <helrond@hotmail.com>
2021-10-25 13:53:59 -0400

    tidying up RecordsContent

--- src/components/RecordsContent/index.js
+++ src/components/RecordsContent/index.js
@@ -143,7 +143,7 @@ export const RecordsChild = props => {
       getPages(
         appendParams(
           `${process.env.REACT_APP_ARGO_BASEURL}${uri}/children/`,
-          {...props.params, limit: 5}
+          {...props.params, limit: pageSize}
         )
       )
     }
@@ -163,7 +163,7 @@ export const RecordsChild = props => {
 
   /* Set isLoading to false once loading has completed */
   useEffect(() => {
-    if (targetElementLoaded || preExpanded.length < 2) {
+    if (targetElementLoaded || preExpanded.length == 1) {
       setIsLoading(false)
     }
   }, [targetElementLoaded, setIsLoading, setIsScrolled, preExpanded])
