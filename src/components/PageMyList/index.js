4e04c1e845308f221628dfd91f73e25dd8265357
Kevin Cloud <kmc247@pitt.edu>
2021-12-10 17:20:55 -0500

    Revert to hardcoded form value

--- src/components/PageMyList/index.js
+++ src/components/PageMyList/index.js
@@ -117,7 +117,7 @@
         .post(uri, submitted)
         .then(res => {
           const form = document.createElement('form')
-          form.action = `${process.env.AEON_URL}`
+          form.action = 'https://pitt.aeon.atlas-sys.com/aeon/aeon.dll'
           form.method = 'post'
           Object.keys(res.data).forEach(key => {
             if (Array.isArray(res.data[key])) {
