858b43d36d269b3ea77d829fbac5256ce4852e96
Hillel Arnold <helrond@hotmail.com>
2022-08-03 16:47:45 -0400

    add request data

--- src/components/PageBackendError/index.js
+++ src/components/PageBackendError/index.js
@@ -16,6 +16,7 @@ const PageBackendError = ({error}) => (
       <h1 className='backend-error__title'>There was an error fetching data.</h1>
       <p className='backend-error__text'>The request to {error.config.url} failed.</p>
       <p className='backend-error__text'>The error message was {error.code}: {error.message}</p>
+      {error.request.data ? <p className='backend-error__text'>Request data: ${error.request.data}</p> : null}
       <p className='backend-error__text'>To report this problem, send us an email at <a href='mailto:archive.rockarch.org'>archive.rockarch.org</a>.</p>
     </div>
   </>
