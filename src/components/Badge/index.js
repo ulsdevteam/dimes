378cdaadf219176ca58b7e606e5e59b412dc3b66
Hannah <hsistrunk@gmail.com>
2022-09-12 14:21:43 -0400

    remove redundant badge styles

--- src/components/Badge/index.js
+++ src/components/Badge/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
-import './styles.scss'
 
 export const Badge = ({ className, text }) => (
   <span className={classnames('badge', className)}>
