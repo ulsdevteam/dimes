fe3738bc9765b88469f6ef8a112b6e6bdc5f2089
Hillel Arnold <hillel.arnold@gmail.com>
2022-12-15 12:45:05 -0500

    proof of concept

---
+++ backstop_data/engine_scripts/mock_request.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+
+function createFilepath(label, url, method) {
+  const labelSanitize = label.replace(/\s/g, "_").toLowerCase()
+  const urlSanitize = url.replace("http://", "").replace("https://", "").replace(/\//g, "_")
+  return `backstop_data/request_reference/${labelSanitize}__${urlSanitize}__${method.toLowerCase()}`
+}
+
+module.exports = async (page, scenario, vp, isReference) => {
+  await require('./puppet/loadCookies')(page, scenario);
+  
+  await page.setRequestInterception(true);
+
+  page.on("request", req => {
+    const fixturePath = createFilepath(scenario.label, req.url(), req.method())
+    if (req.method() === 'OPTIONS') {
+      req.respond({
+        status: 200,
+        headers: {
+          'access-control-allow-headers': 'accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with',
+          'access-control-allow-methods': 'DELETE, GET, OPTIONS, PATCH, POST, PUT',
+          'access-control-allow-origin': '*',
+          'access-control-max-age': '86400',
+          'content-length': '0',
+          'content-type': 'text/html; charset=utf-8',
+        }
+      })
+    } else if (fs.existsSync(fixturePath)) {
+      const stubText = fs.readFileSync(fixturePath, {encoding:'utf8', flag:'r'})
+      const stubResponse = JSON.parse(stubText)
+      if (stubResponse) {
+        req.respond({
+          status: 200,
+          content: 'application/json',
+          headers: {"Access-Control-Allow-Origin": "*"},
+          body: JSON.stringify(stubResponse)
+        });
+      }
+    } else {
+      console.log(fixturePath)
+      req.continue();
+    }
+  })
+};
