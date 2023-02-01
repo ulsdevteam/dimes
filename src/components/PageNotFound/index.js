77c6afce10fcf9ae981c4a8ab15369943febeb3c
Kevin Cloud <kmc247@pitt.edu>
2023-02-01 17:11:58 -0500

    Remove search link on not found page

--- src/components/PageNotFound/index.js
+++ src/components/PageNotFound/index.js
@@ -13,7 +13,6 @@ const PageNotFound = () => (
     <div className='not-found'>
       <span className='not-found__icon'><MaterialIcon icon='help_outline' /></span>
       <h1 className='not-found__header'>Sorry, the requested page was not found!</h1>
-      <p className='not-found__text'>Try <a href='/'>a search</a> to find what you're looking for.</p>
     </div>
   </>
 )
