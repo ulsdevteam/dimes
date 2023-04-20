e67d663f2dd133481fd3c50a3c5c76ba9db4ea53
Hannah <hsistrunk@gmail.com>
2023-04-20 17:07:36 -0400

    disable transitions and wait for fonts to load

--- backstop_data/engine_scripts/puppet/onReady.js
+++ backstop_data/engine_scripts/puppet/onReady.js
@@ -2,5 +2,10 @@ module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
   await require('./clickAndHoverHelper')(page, scenario);
 
-  // add more ready handlers here...
+  // Wait for fonts to load
+  await page.evaluateHandle('document.fonts.ready');
+
+  // Disable CSS transitions to avoid layout shifts being delayed
+  const fs = require("fs")
+  await page.addStyleTag({ content: fs.readFileSync("backstop_data/engine_scripts/backstop.css", "utf-8") });
 };
