c22085ad905fe676c523dddf57389f758b0ce49e
Hannah <hsistrunk@gmail.com>
2023-05-19 14:42:55 -0400

    user correct image link

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,7 +4,7 @@ import './styles.scss'
 const Hero = () => (
   <div className='hero'>
     <div className='hero__logo'>
-      <img alt='' src='https://assets.rockarch.org/assets/img/hero_logo.svg' />
+      <img alt='' src='https://assets.rockarch.org/assets/img/hero_logo.png' />
     </div>
     <h1 className='hero__text'>
       Search Our Collections.<br />Discover People and Organizations.<br />Access Digital Content.
