34236aa26cd92d6c85de3d77b991fc84cd056be4
Hannah <hsistrunk@gmail.com>
2023-06-23 08:33:54 -0400

    Merge branch 'development' into issue-615

---
+++ src/__fixtures__/parsedBatch.js
@@ -0,0 +1,10 @@
+export const parsedBatch = [{
+    "uri": "/repositories/2/archival_objects/1053534",
+    "submit": true,
+    "submit_reason": null
+    },
+    {
+    "uri": "/repositories/2/archival_objects/1053535",
+    "submit": true,
+    "submit_reason": null
+}]
