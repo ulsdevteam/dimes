76784cae66678490e6d118486230628408b91749
Kevin Cloud <kmc247@pitt.edu>
2023-07-14 17:50:55 -0400

    Backup changes added 7/14/2023

---
+++ src/components/ModalMinimap/__tests__/ModalMinimapInfo.test.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import { ModalMinimapInfo } from '..'
+
+let container = null
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
+
+it('renders props correctly', () => {
+  act(() => {
+    render(<ModalMinimapInfo
+      appElement={container}
+      hasSeenMinimapIntro={false}
+      isOpen
+      toggleModal={jest.fn()} />, container)
+  })
+})
+
+it('handles clicks correctly', () => {
+  const toggleModal = jest.fn()
+
+  act(() => {
+    render(<ModalMinimapInfo
+      appElement={container}
+      hasSeenMinimapIntro={false}
+      isOpen
+      toggleModal={toggleModal} />, container)
+  })
+
+  const button = document.querySelector('.modal-header__button')
+
+  act(() => {
+    button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+  })
+
+  expect(toggleModal).toHaveBeenCalledTimes(1)
+})
