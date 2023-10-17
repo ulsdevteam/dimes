48a532e709a6dfcdcc5ceaee7af3192e842d032f
Patrick <pjg250@gmail.com>
2023-10-17 09:26:26 -0400

    Adds Spanish
    Adds a Spanish translation

--- lingui.config.js
+++ lingui.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@lingui/conf').LinguiConfig} */
 module.exports = {
-	locales: ["en", "it", "pt"],
+	locales: ["en", "es", "it", "pt"],
 	sourceLocale: "en",
 	catalogs: [{
 		path: "src/locales/{locale}/messages",
