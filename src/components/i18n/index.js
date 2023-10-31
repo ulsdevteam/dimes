681d14680832117fcb506a692324fb7e9fac5a3e
Patrick <pjg250@gmail.com>
2023-10-31 10:01:45 -0400

    Fix minor translation errors and add message loading
    Loads messages for all translations and fixes minor translation errors.

--- src/components/i18n/index.js
+++ src/components/i18n/index.js
@@ -1,8 +1,29 @@
 import { I18nProvider } from '@lingui/react'
 import { i18n } from '@lingui/core'
 import { messages } from '../../locales/en/messages'
+import { messages as cnMessages } from '../../locales/cn/messages'
+import { messages as deMessages } from '../../locales/de/messages'
+import { messages as enMessages } from '../../locales/en/messages'
+import { messages as esMessages } from '../../locales/es/messages'
+import { messages as frMessages } from '../../locales/fr/messages'
+import { messages as itMessages } from '../../locales/it/messages'
+import { messages as jaMessages } from '../../locales/ja/messages'
+import { messages as koMessages } from '../../locales/ko/messages'
+import { messages as ptMessages } from '../../locales/pt/messages'
+import { messages as trMessages } from '../../locales/tr/messages'
 
-i18n.load('en', messages)
+i18n.load({
+  cn: cnMessages,
+  de: deMessages,
+  en: enMessages,
+  es: esMessages,
+  fr: frMessages,
+  it: itMessages,
+  ja: jaMessages,
+  ko: koMessages,
+  pt: ptMessages,
+  tr: trMessages,
+});
 
 i18n.activate('en')
 
