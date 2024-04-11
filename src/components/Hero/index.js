3aae338764077e98777372e3d99e24f42e6b9ac4
Tyler Lendon <trl75@pitt.edu>
2024-04-11 09:57:26 -0400

    Pitt Styling from css, fonts, and images

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,7 +5,7 @@ import './styles.scss'
 const Hero = () => (
   <div className='hero'>
     <div className='hero__logo'>
-      <img alt='' src='https://assets.rockarch.org/assets/img/hero_logo.png' />
+      <img alt='' src='logo.svg' />
     </div>
     <h1 className='hero__text'>
       <Trans comment='Message shown within the Hero'>
