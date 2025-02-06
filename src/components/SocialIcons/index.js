aa547cc270cb97d7f6b6dfae28c4c69d6815b75e
Clinton Graham <ctgraham@pitt.edu>
2025-02-06 14:16:18 -0500

    revert locale changes to upstream

--- src/components/SocialIcons/index.js
+++ src/components/SocialIcons/index.js
@@ -42,12 +42,12 @@ const SocialIcons = () => (
         comment: 'Youtube account link',
         message: 'https://www.youtube.com/channel/UCks9ctz4OF9tMNOTrRkWIZg'
       })} aria-label='YouTube'>
-      <span className='social-icons__icon'>
+        <span className='social-icons__icon'>
           <svg aria-hidden='true' focusable='false' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
             <title>Tumblr: Archives &amp; Manuscripts @ Pitt</title>
             <path fill='#192E49' d='M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z' />
           </svg>
-        </span> 
+        </span>
       </a>
       <a href={t({
         comment: 'Flickr account link',
